ac028f0f7f708b68078a16cf8584acfe52755e69
timlaurijs <timlaurijs@gmail.com>
2020-08-18 17:22:53 +0200

    removed unwanted items

--- schemas/schema.js
+++ schemas/schema.js
@@ -6,9 +6,8 @@ import schemaTypes from "all:part:@sanity/base/schema-type"
 
 // We import object and document schemas
 import blockContent from "./blockContent"
-import season from "./season"
 import post from "./post"
-import imageFeed from "./imageFeed"
+import menuItem from "./menuItem"
 
 // Then we give our schema to the builder and provide the result to Sanity
 export default createSchema({
@@ -17,11 +16,8 @@ export default createSchema({
   // Then proceed to concatenate our document type
   // to the ones provided by any plugins that are installed
   types: schemaTypes.concat([
-    // The following are document types which will appear
-    // in the studio.
+    menuItem,
     post,
-    season,
-    imageFeed,
     // When added to this list, object types can be used as
     // { type: 'typename' } in other document schemas
     blockContent,
